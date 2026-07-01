5169936ca7a17da4618f99c6d332dc58559c4d92
benasphy <binidani1903@gmail.com>
2026-07-01 11:39:29 +0300

    add hyperparameters

--- temporal_difference_learning.py
+++ temporal_difference_learning.py
@@ -16,3 +16,10 @@ class EnvironmentBlackBox:
         next_col = max(0, min(2, col + d_col))
         next_state = next_row * 3 + next_col
         return next_state, -1, (next_state == self.terminal_state)
+
+# Shared Hyperparameters
+gamma = 1.0
+alpha = 0.1  # Learning rate
+epsilon = 0.1
+episodes = 2000
+env = EnvironmentBlackBox()
