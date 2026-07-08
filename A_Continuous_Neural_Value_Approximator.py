b56d4f0e0bee454a5de7fd9fb66f3fa9a693d3ef
benasphy <binidani1903@gmail.com>
2026-07-08 14:51:38 +0300

    initialize a function

--- A_Continuous_Neural_Value_Approximator.py
+++ A_Continuous_Neural_Value_Approximator.py
@@ -6,3 +6,8 @@ import numpy as np
 # Set random seeds for reproducibility
 torch.manual_seed(42)
 np.random.seed(42)
+
+class ContinuousLineEnv:
+    def __init__(self):
+        self.state = 0.0
+        self.goal = 1.0
