54880eda4d3fbf979f665e12fd5ec124febb5f68
benasphy <binidani1903@gmail.com>
2026-07-12 02:35:34 +0300

    step function

--- REINFORCE_with_Baseline.py
+++ REINFORCE_with_Baseline.py
@@ -12,3 +12,11 @@ class DiscreteLineEnv:
     def reset(self):
         self.state = 0.0
         return np.array([self.state], dtype=np.float32)
+    
+    def step(self, action):
+        if action == 1: # Move
+            self.state += np.random.uniform(0.1, 0.2)
+        # Action 0 is 'Stay', keeping the state identical
+        self.state = min(self.goal, self.state)
+        reward = -1.0 if self.state < self.goal else 0.0
+        return np.array([self.state], dtype=np.float32), reward, (self.state >= self.goal)
