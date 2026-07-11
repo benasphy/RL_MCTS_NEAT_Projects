9dc4cac4821e581777f1153418e64acc56ab8115
benasphy <binidani1903@gmail.com>
2026-07-11 12:47:30 +0300

    step function

--- Dueling_Double_DQN.py
+++ Dueling_Double_DQN.py
@@ -22,3 +22,9 @@ class DiscreteLineEnv:
     def reset(self):
         self.state = 0.0
         return np.array([self.state], dtype=np.float32)
+
+    def step(self, action):
+        if action == 1: self.state += np.random.uniform(0.05, 0.15)
+        self.state = min(self.goal, self.state)
+        reward = -1.0 if self.state < self.goal else 0.0
+        return np.array([self.state], dtype=np.float32), reward, (self.state >= self.goal)
