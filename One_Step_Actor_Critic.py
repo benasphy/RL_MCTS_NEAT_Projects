b726c6af8a8e6bd6805fb039e5f45a3e8cb3c798
benasphy <binidani1903@gmail.com>
2026-07-12 03:00:39 +0300

    step function

--- One_Step_Actor_Critic.py
+++ One_Step_Actor_Critic.py
@@ -12,3 +12,10 @@ class DiscreteLineEnv:
     def reset(self):
         self.state = 0.0
         return np.array([self.state], dtype=np.float32)
+    
+    def step(self, action):
+        if action == 1: # Move
+            self.state += np.random.uniform(0.1, 0.2)
+        self.state = min(self.goal, self.state)
+        reward = -1.0 if self.state < self.goal else 0.0
+        return np.array([self.state], dtype=np.float32), reward, (self.state >= self.goal)
