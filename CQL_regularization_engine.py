ab9b86ae53e5f69656ad1606508a3fcec2b689ea
benasphy <binidani1903@gmail.com>
2026-07-17 12:03:53 +0300

    init function

--- CQL_regularization_engine.py
+++ CQL_regularization_engine.py
@@ -1,3 +1,14 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
+
+class QNetwork(nn.Module):
+    def __init__(self, state_dim=2, action_dim=3):
+        super(QNetwork, self).__init__()
+        self.fc = nn.Sequential(
+            nn.Linear(state_dim, 32),
+            nn.ReLU(),
+            nn.Linear(32, action_dim) # Outputs Q-value for each discrete action
+        )
+    def forward(self, state):
+        return self.fc(state)
